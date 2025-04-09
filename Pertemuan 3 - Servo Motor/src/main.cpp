9996a85eee273e14668e2b7308f34b60633265b1
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2025-04-09 11:20:02 +0700

    Servo Motor

---
+++ Pertemuan 3 - Servo Motor/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+
+Servo myServo;
+
+
+void setup() {
+  myServo.attach(D4);
+}
+
+void loop() {
+  // for(int pos = 0; pos <= 180; pos ++) {
+  //   myServo.write(pos);
+  //   delay(10);
+  // }
+
+  // for(int pos = 180; pos >= 0; pos --) {
+  //   myServo.write(pos);
+  //   delay(10);
+  // }
+
+  myServo.write(180);
+  delay(2000);
+  myServo.write(0);
+  delay(2000);
+  myServo.write(180);
+  delay(2000);
+  
+}
